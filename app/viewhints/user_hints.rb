c5438a85b2e56ed4548cf18d3b8bd154e3d52503
Tom Locke <tom@tomlocke.com>
2010-03-29 15:20:06 +0100

    On signup, tell the user we won't share their email

--- app/viewhints/user_hints.rb
+++ app/viewhints/user_hints.rb
@@ -2,7 +2,7 @@ class UserHints < Hobo::ViewHints
 
   children :films
   
-  field_help :email       => "You'll need to collect email from here to finish your submission, so please use a real account!",
+  field_help :email       => "You'll need to collect email from here to finish your submission, so please use a real account! <b>Your email address will not be shared with other organisations</b>, and will be used to contact you only regarding SciCast.",
              :institution => "Your school, university, company - leave blank if it doesn't apply",
              :feedback    => "How's SciCast been for you? Enjoyed it? Found it useful? Think there's something we can do better? Please let us know!",
              :password    => "Choose a password for your SciCast account; you'll need it to complete your submission, and to check its progress through the system.",
