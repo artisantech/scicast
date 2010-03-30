1dad69e907bfb2953ff19f50562ee3c10d1987af
Tom Locke <tom@tomlocke.com>
2010-03-30 15:43:18 +0100

    Another tweak to team-info help text

--- app/viewhints/film_hints.rb
+++ app/viewhints/film_hints.rb
@@ -5,7 +5,7 @@ class FilmHints < Hobo::ViewHints
              :production_date => "It's useful for us to know when you made your film, but whatever you enter here won't affect your entry into the competition.",
              :license         => "We need your permission to publish your film, and use <a target='_blank' href='http://creativecommons.org'>Creative Commons</a> licenses for this. We prefer the CC 'BY' license, but if you'd rather restrict commercial use of your film, choose 'BY NC SA' here. Read <a target='_blank' href='http://www.planet-scicast.com/film_school/production_-_licensing.cfm'>our notes on the choice</a>.",
              :team_name       => "We'll use this to credit your team throughout the site.",
-             :team_info       => "How many people were in the team? What ages were they? Did any adults actually help make the film, if so who and how?  (Please also tell us if any of the adults who helped are involved in any aspect of film-making professionally, who and what aspect?)",
+             :team_info       => "How many people were in the team? What ages were they? Did any adults help make the film, if so who and how? Or tell us if it's an all adult team.",
              :others_material => "Have you used anyone else's material in your film (music, photos, video clips)? If so, tell us here - give us as much information as you can. Ideally, include a link to the web page for the material."
   
   field_names :others_material => "Material used"
