3de8c271bb3bf8eae792debed0839a0c452ece91
Tom Locke <tom@tomlocke.com>
2010-04-16 14:32:35 +0100

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,20 @@ ActiveRecord::Schema.define(:version => 20100416090912) do
 
   add_index "films", ["user_id"], :name => "index_films_on_user_id"
 
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type"], :name => "index_taggings_on_taggable_id_and_taggable_type"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "crypted_password",          :limit => 40
     t.string   "salt",                      :limit => 40
