5ad9af380e30e11b7118e863d14f865b8f27dc7f
Tom Locke <tom@tomlocke.com>
2010-04-20 13:07:54 +0100

    Allow admins to delete films

--- app/controllers/admin/films_controller.rb
+++ app/controllers/admin/films_controller.rb
@@ -2,7 +2,7 @@ class Admin::FilmsController < Admin::AdminSiteController
 
   hobo_model_controller
 
-  auto_actions :index, :show, :edit, :update
+  auto_actions :index, :show, :edit, :update, :destroy
   
   def index
     if params[:tags]
