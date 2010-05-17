6d4b282ff7b9e9577eb6d653ee98e722502e6276
Tom Locke <tom@tomlocke.com>
2010-05-17 14:45:01 +0100

    IE8 fix for playing movies in the judging site

--- public/javascripts/judging.js
+++ public/javascripts/judging.js
@@ -1,9 +1,5 @@
-document.observe("dom:loaded", function() {
-  $$('body.category.show-page .movie-icon.enabled').each(function(el) {
-    Event.observe(el, "click", function() {
-      var player = $('player').innerHTML.replace(/\[MOVIE-SRC\]/g, this.getAttribute('movie-url'))
-      var button = "<button class='hider' onclick='this.parentNode.innerHTML=\"\"'>Hide</button>"
-      $(this).up('.card').down('.player-placeholder').innerHTML = player + button
-    })
-  });
-});
+function playMovie(el) {
+  var player = $('player').innerHTML.replace(/\[MOVIE-SRC\]/g, el.getAttribute('movie-url'));
+  var button = "<button class='hider' onclick='this.parentNode.innerHTML=\"\"'>Hide</button>";
+  $(el).up('.card').down('.player-placeholder').innerHTML = player + button;
+}
