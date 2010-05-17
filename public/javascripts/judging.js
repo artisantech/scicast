0d2930c34f733a4a4dc055b203aa39e7462027c8
Tom Locke <tom@tomlocke.com>
2010-05-17 13:26:13 +0100

    Fix for IE8 compatibility for judging site video players

--- public/javascripts/judging.js
+++ public/javascripts/judging.js
@@ -1,12 +1,9 @@
 document.observe("dom:loaded", function() {
-  $$('body.category.show-page .movie-icon.enabled').each(function(el) {});
-});
-
-Event.addBehavior({
-    
-    'body.category.show-page .movie-icon.enabled:click' : function() {
+  $$('body.category.show-page .movie-icon.enabled').each(function(el) {
+    Event.observe(el, "click", function() {
       var player = $('player').innerHTML.replace(/\[MOVIE-SRC\]/g, this.getAttribute('movie-url'))
-      $(this).up('.card').down('.player-placeholder').innerHTML = player + "<button class='hider' onclick='this.parentNode.innerHTML=\"\"'>Hide</button>"
-    }
-    
-})
+      var button = "<button class='hider' onclick='this.parentNode.innerHTML=\"\"'>Hide</button>"
+      $(this).up('.card').down('.player-placeholder').innerHTML = player + button
+    })
+  });
+});
