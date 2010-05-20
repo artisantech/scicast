c52611b155b00c0cd06b2d341f589bdc91da483d
Tom Locke <tom@tomlocke.com>
2010-05-20 12:30:38 +0100

    Added download link to movies in judging site

--- public/javascripts/judging.js
+++ public/javascripts/judging.js
@@ -1,5 +1,6 @@
 function playMovie(el) {
   var player = $('player').innerHTML.replace(/\[MOVIE-SRC\]/g, el.getAttribute('movie-url'));
   var button = "<button class='hider' onclick='this.parentNode.innerHTML=\"\"'>Hide</button>";
-  $(el).up('.card').down('.player-placeholder').innerHTML = player + button;
+  var download = " <a href='" + el.getAttribute('movie-url') + "'>Download</a> (right click link to download)"
+  $(el).up('.card').down('.player-placeholder').innerHTML = '<div class="film-actions">' + player + button + download + "</div>"
 }
