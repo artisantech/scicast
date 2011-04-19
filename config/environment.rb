b31590066662beda2a5a3d718aed213f4662f797
Tom Locke <tom@tomlocke.com>
2011-04-19 09:34:34 +0100

    Fix Hobo version to 1.0.0

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'hobo'
+  config.gem 'hobo', :version => "1.0.0"
   config.gem 'geokit'
 
   # Settings in config/environments/* take precedence over those specified here.
